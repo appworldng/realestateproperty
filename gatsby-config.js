57ddfb556fa5e42f184f32fff561ec359c547664
chigozieorunta <chigozieorunta@yahoo.com>
2020-09-14 12:44:22 +0100

    Set up Poppins font for web project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`poppins:300,400,500,600,700`],
+        display: "swap",
+      },
+    },
+  ],
 }
